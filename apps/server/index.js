729a8443ff9e391fd51ed4805eec393315b0a4d5
Sergei Shilov <ya.sergei240300@yandex.ru>
2025-03-30 08:14:31 +0300

    try to avoid couldn't locate bindings err

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-// import sqlite3 from 'sqlite3';
+import sqlite3 from 'sqlite3';
 import path from 'path';
 import fs from 'fs/promises';
 import { fileURLToPath } from 'url';
@@ -19,43 +19,43 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-// const dbPath = path.resolve(__dirname, '../../db/fiona.db');
+const dbPath = path.resolve(__dirname, '../../db/fiona.db');
 
-// // Подключение к базе данных SQLite
-// const db = new sqlite3.Database(dbPath, (err) => {
-//   if (err) {
-//     console.error('Ошибка подключения к БД:', err.message);
-//     process.exit(1); // Завершаем процесс, если не удалось подключиться
-//   } else {
-//     console.log('Подключение к SQLite установлено.');
-//   }
-// });
+// Подключение к базе данных SQLite
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Ошибка подключения к БД:', err.message);
+    process.exit(1); // Завершаем процесс, если не удалось подключиться
+  } else {
+    console.log('Подключение к SQLite установлено.');
+  }
+});
 
 // Чтение и выполнение SQL-скрипта для инициализации таблиц
-// const initSqlPath = path.resolve(__dirname, '../../db/init.sql');
+const initSqlPath = path.resolve(__dirname, '../../db/init.sql');
 
-// (async () => {
-//   try {
-//     const initSql = await fs.readFile(initSqlPath, 'utf-8');
-//     db.exec(initSql, (err) => {
-//       if (err) {
-//         console.error('Ошибка выполнения SQL-скрипта:', err.message);
-//       } else {
-//         console.log('Таблицы успешно созданы.');
-//       }
-//     });
-//   } catch (err) {
-//     console.error('Ошибка чтения файла init.sql:', err.message);
-//     process.exit(1);
-//   }
-// })();
+(async () => {
+  try {
+    const initSql = await fs.readFile(initSqlPath, 'utf-8');
+    db.exec(initSql, (err) => {
+      if (err) {
+        console.error('Ошибка выполнения SQL-скрипта:', err.message);
+      } else {
+        console.log('Таблицы успешно созданы.');
+      }
+    });
+  } catch (err) {
+    console.error('Ошибка чтения файла init.sql:', err.message);
+    process.exit(1);
+  }
+})();
 
 app.use(express.json());
 
 // Передача подключения `db` в маршруты
-// app.use('/api/transactions', transactionsRoutes(db));
-// app.use('/api/auth', authRoutes(db));
-// app.use('/api/users', usersRoutes(db));
+app.use('/api/transactions', transactionsRoutes(db));
+app.use('/api/auth', authRoutes(db));
+app.use('/api/users', usersRoutes(db));
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
